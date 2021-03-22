b3a3cd6c87d00fbfd0117876362cb03d138e8428
Filip Giermek <76012392+filipgiermo22@users.noreply.github.com>
2021-03-22 20:58:15 +0100

    Introduction to Windows Powershell ISE

---
+++ lab1/sito.ps1
@@ -0,0 +1,22 @@
+﻿$num = Read-Host
+Write-Host $num
+
+$arr =@($true) * $num
+
+write-host("array values: ")
+
+for($i = 2; $i -lt $num;$i++)
+{
+    for ($j = $i + $i; $j -lt $num; $j = $j + $j)
+    {
+    $arr[$j] = @($false);
+    }
+}
+
+for ($i =2; $i -lt $num; $i++)
+{
+    if ($arr[$i] -eq $true)
+    {
+    write-host($i)
+    }
+}
