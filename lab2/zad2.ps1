39bf4261eeee3977167b8a21646c497ebabb9e4f
Filip Giermek <76012392+filipgiermo22@users.noreply.github.com>
2021-03-22 21:00:56 +0100

    pipeline introduction, output formatting

---
+++ lab2/zad2.ps1
@@ -0,0 +1,12 @@
+﻿while($true) 
+{
+    get-date -format "HH:mm:ss" |
+    
+    out-file -filepath .\czas.txt -append -width 200
+
+    get-content -path .\czas.txt -tail 1
+    
+    sleep 5
+}
+
+
---
+++ lab2/zad3.ps1
@@ -0,0 +1,16 @@
+﻿while($true) 
+{
+    $process = get-process | sort cpu -descending | select name, cpu -first 1
+    
+    $time =  get-date -format "HH:mm:ss"
+
+    $result = $time + " " + $process.Name + " " + $process.CPU
+    
+    $result | out-file -filepath .\CPU.txt -append -width 200
+    
+    get-content -path .\CPU.txt -tail 1
+    
+    sleep 5
+}
+
+
